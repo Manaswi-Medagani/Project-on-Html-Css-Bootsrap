63fd6719ab1aea7edc66502eab534311bf3ded1e
Manaswi-Medagani <medaganimanaswi@gmail.com>
2024-09-18 21:20:56 +0530

    my first project

---
+++ firs prgm  - Copy.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>document</title>
+    <link rel="stylesheet"  href="/.js" >
+</head>
+<body>
+    var username="manaswi"
+        if(username==="manaswi"){
+            console.log("true")
+        }
+    else if( password==="manu"){
+        console.log("false")
+    }
+    else{
+        console.log(" details not avaiable")
+    }
+    
